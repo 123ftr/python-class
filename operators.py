464e5e08e81f14d6865aa8adf515d275c1a462f0
123ftr <97871422+123ftr@users.noreply.github.com>
2022-01-24 10:27:57 +0300

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,25 @@
+#opertors
+a=5
+b=5
+c=a+b #addition
+d=8
+e=9
+f=e-d # subtraction
+g=78
+h=9
+i=g*h # multiplication
+j=4
+k=8
+l= k/j #division
+m=6
+n=5
+o=n**m #power
+p=7
+q=5
+r=p%q #remainder
+print(c)
+print(f)
+print(i)
+print(l)
+print(o)
+print(r)
---
+++ type.py
@@ -0,0 +1,9 @@
+#type()
+a=78 #int
+b=67.9 #float
+c="small" #str
+d=3+2j #complex
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
